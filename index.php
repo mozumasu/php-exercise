b101c9fc23543f36afeef9982eaf813779dbba7e
mozumasu <ouri1229@gmail.com>
2024-05-01 08:49:11 +0900

    feat: 🎸 フォームで配列を送る

--- index.php
+++ index.php
@@ -19,3 +19,20 @@
   <br>
   <button type="submit">POSTメソッド</button>
 </form>
+
+<!-- フォームで配列を送る -->
+<form action="receive.php">
+  <div>
+    <label for="account[id]">ID</label>
+    <input type="text" name="account[id]">
+  </div>
+  <div>
+    <label for="account[name]">name</label>
+    <input type="text" name="account[name]">
+  </div>
+  <div>
+    <label for="account[pwd]">password</label>
+    <input type="text" name="account[pwd]">
+  </div>
+ <input type="submit" value="配列を送信">
+</form>

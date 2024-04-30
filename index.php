837f96084a917dc92891bda5ef34789c6b6263e8
mozumasu <ouri1229@gmail.com>
2024-04-30 16:38:41 +0900

    feat: 🎸 Getメソッド

---
+++ index.php
@@ -0,0 +1,9 @@
+<form action="get.php" method="GET">
+  <label for="username">username : </label>
+  <input type="text" name="username">
+  <br>
+  <label for="pwd">pwd : </label>
+  <input type="password" name="pwd">
+  <br>
+  <button type="submit">送信</button>
+</form>
